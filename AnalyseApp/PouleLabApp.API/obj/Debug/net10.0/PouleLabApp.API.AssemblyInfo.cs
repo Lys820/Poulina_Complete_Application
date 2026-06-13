37f2b5cea88f155a85eba71540dc3c3c4ae6d57b
Lilia_BD <lilia.ben-dai@etu.iut-tlse3.fr>
2026-06-13 19:17:43 +0100

    Correction 12 - Notification

--- AnalyseApp/PouleLabApp.API/obj/Debug/net10.0/PouleLabApp.API.AssemblyInfo.cs
+++ AnalyseApp/PouleLabApp.API/obj/Debug/net10.0/PouleLabApp.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PouleLabApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0208b42663244afa6b55d325235085fe67f5d266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5d12cd59272f5ff23734ecf8f188188bee74f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PouleLabApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PouleLabApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
